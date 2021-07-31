13c7c9d69b6d785c1dcc7421b5dfabc62451438f
sean-mcelwain <79949547+sean-mcelwain@users.noreply.github.com>
2021-07-30 19:45:06 -0500

    updated workout.js with day and exercises

--- models/workout.js
+++ models/workout.js
@@ -2,7 +2,42 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-
+    day: {
+        type: Date,
+        default: Date.now,
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+            },
+            name: {
+                type: String,
+                trim: true,
+            },
+            duration: {
+                type: Number,
+                trim: true,
+            },
+            distance: {
+                type: Number,
+                trim: true,
+            },
+            weight: {
+                type: Number,
+                trim: true,
+            },
+            reps: {
+                type: Number,
+                trim: true,
+            },
+            type: {
+                type: Number,
+                trim: true,
+            },
+        }
+    ]
 
 });
 
