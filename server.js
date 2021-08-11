7110413f5b5c5d43c41bb4445f9b1e3d86187c7c
sean-mcelwain <79949547+sean-mcelwain@users.noreply.github.com>
2021-08-10 21:15:36 -0500

    debugging for deployment

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
-const morgan = require('morgan');
+const compression = require("compression");
 
 const PORT = process.env.PORT || 3000;
+
 const app = express();
 
+app.use(logger("dev"));
+
+app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use(express.static("public"));
-app.use(morgan('dev'));
 
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/Workout_Tracker", 
